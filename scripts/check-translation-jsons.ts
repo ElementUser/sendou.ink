be3592dc71abbc86b4c55e48e42f422ae2b083b8
William Lam <william.lam.business@gmail.com>
2024-08-25 13:10:40 -0400

    Quotation mark linting

--- scripts/check-translation-jsons.ts
+++ scripts/check-translation-jsons.ts
@@ -3,7 +3,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-const biome = require('@biomejs/biome');
+const biome = require("@biomejs/biome");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
